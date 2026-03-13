90ada35daad0ae549a187ba212138093b464b745
Emmanuel Masangkay <manumasangkay0603@gmail.com>
2026-03-13 16:21:28 +0800

    Added 3 Layers
    Added
    Data Layer
    Business Logic Layer
    Model Layer

---
+++ LoadModelLayer/LoadModelLayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RALPH_LOAD_MANAGEMENT.LoadModelLayer
+{
+    internal class Class1
+    {
+    }
+}
