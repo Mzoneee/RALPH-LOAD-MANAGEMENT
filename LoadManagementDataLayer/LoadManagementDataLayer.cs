2c0c6f36bee4c6c97a5abf43b56f16f955da8112
Ralph Aaron A. Monzon <monzonralphaaronpogi@gmail.com>
2026-03-13 18:34:49 +0800

    Update LoadManagementDataLayer.cs

--- LoadManagementDataLayer/LoadManagementDataLayer.cs
+++ LoadManagementDataLayer/LoadManagementDataLayer.cs
@@ -1,7 +1,35 @@
-﻿namespace LoadManagementDataLayer
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RALPH_LOAD_MANAGEMENT.LoadModelLayer; 
+
+namespace LoadManagementDataLayer
 {
     public class LoadManagementDataLayer
     {
+        public List<LoadManagementModelLayer> transactions = new List<LoadManagementModelLayer>();
 
-    }
+        public LoadManagementDataLayer()
+        {
+            LoadManagementModelLayer sampleTransaction = new LoadManagementModelLayer
+            {
+                //placeholder values lang
+                TransactionID = Guid.NewGuid().ToString(),
+                PhoneNumber = "09171234567",
+                Network = "Globe",
+                LoadType = "Regular",
+                LoadValue = "50"
+            };
+
+            transactions.Add(sampleTransaction);
+        }
+
+       
+        public void AddTransaction(LoadManagementModelLayer transaction)
+        {
+            transactions.Add(transaction);
+        }
+
+        
+      
 }
