94de98dda17fa435ba43f5153088da99945f8461
Ralph Aaron A. Monzon <monzonralphaaronpogi@gmail.com>
2026-03-13 18:11:26 +0800

    Uploaded Model Layer

---
+++ LoadManagementModelLayer/LoadManagementModelLayer.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RALPH_LOAD_MANAGEMENT.LoadModelLayer
+{
+    public class LoadManagementModelLayer
+    {
+        public string TransactionID { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Network { get; set; }
+        public string LoadType { get; set; }
+        public string LoadValue { get; set; }
+
+
+    }
+}
